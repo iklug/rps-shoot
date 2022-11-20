d95763c4cccd84b92d0a8ff1b797b80fb7465f50
Ian Klug <ianklug123@gmail.com>
2022-11-20 13:02:04 -0800

    add rps ui and JS functionality

--- javascript.js
+++ javascript.js
@@ -1,49 +1,51 @@
-const getComputerChoice = () => {
-    let x = Math.floor(Math.random() * 3)
-    if(x===0){
-        return "Rock";
-    }
-    else if (x===1) {
-        return "Paper";
-    }
-    else if (x===2) {
-        return "Scissors";
-    }
-}
 
-
-
-
-const playRound = (choice1, choice2) => {
+const playRound = choice => {
     const whoBeatsWho = {
         'rock': 0,
         'scissors': 1,
         'paper': 2
     }
-    let myChoice = choice1.toLowerCase();
-    let yourChoice = choice2.toLowerCase();
-    let me = whoBeatsWho[myChoice];
+
+    const getComputerChoice = () => {
+        let x = Math.floor(Math.random() * 3)
+        if(x===0){
+            return "rock";
+        }
+        else if (x===1) {
+            return "paper";
+        }
+        else if (x===2) {
+            return "scissors";
+        }
+    }
+
+    let myChoice = choice;
+    let yourChoice = getComputerChoice();
+
+    console.log(myChoice, yourChoice);
+    let me = whoBeatsWho[choice];
     let you = whoBeatsWho[yourChoice];
+
     if (me === you) {
-        return `It's a tie!`;
+        return `It's a tie! Go again.`;
     } 
     else if (me === 0 && you === 1){
-        return `You win! ${myChoice} beats ${yourChoice}`;
+        return `Your point! ${myChoice} beats ${yourChoice}`;
     }
     else if (me === 0 && you === 2){
-        return `You lose! ${yourChoice} beats ${myChoice}`;
+        return `Sorry! ${yourChoice} beats ${myChoice}`;
     }
     else if (me === 1 && you === 0){
-        return `You lose! ${yourChoice} beats ${myChoice}`;
+        return `Sorry! ${yourChoice} beats ${myChoice}`;
     }
     else if (me === 1 && you === 2){
-        return `You win! ${myChoice} beats ${yourChoice}`;
+        return `Your point! ${myChoice} beats ${yourChoice}`;
     }
     else if  (me === 2 && you === 0){
-        return `You win! ${myChoice} beats ${yourChoice}`;
+        return `Your point! ${myChoice} beats ${yourChoice}`;
     }
     else if (me === 2 && you === 1){
-        return `You lose! ${yourChoice} beats ${myChoice}`;
+        return `Sorry! ${yourChoice} beats ${myChoice}`;
     }
 }
 
@@ -52,43 +54,169 @@ const ref = string => {
     if (string.includes('tie')){
         return 'tie';
     }
-    else if (string.includes('win')){
+    else if (string.includes('point')){
         return 'win';
     }
-    else if (string.includes('lose')){
+    else if (string.includes('Sorry')){
         return 'lose';
     }
 }
 
-const game = () => {
+let yourScore = 1;
+let computerScore = 1;
 
-    
-let myPoints = 0;
-let yourPoints = 0;
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', (e) => {
+    e.target.classList.add('pushed');
+    const messageDiv = document.querySelector('.results');
 
-for (let i = 0; i < 5; i++){
-// let rapidfire = prompt(`Choice ${i + 1} out of 5`)
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-let round = playRound(rapidfire, computerSelection);
-    
-    
-    if (ref(round) === 'win'){
-        myPoints++;
+        if(document.querySelector('.results>p')){
+        messageDiv.removeChild(document.querySelector('.results>p'));
+        };
+
+    const message = document.createElement('p');
+    message.textContent = playRound('rock');
+
+    if (ref(message.textContent) === 'win'){
+        let yourPoints = document.querySelector('.points-left');
+
+        yourPoints.textContent = '0' + `${yourScore}`;
+        yourScore++;
+        if(yourScore === 6){
+        message.textContent = 'You win the game!'
+        }
+
+    }      
+    if (ref(message.textContent) === 'lose'){
+        let compPoints = document.querySelector('.points-right');
+
+        compPoints.textContent = '0' + `${computerScore}`;
+        computerScore++;
+
+        if(computerScore === 6){
+        message.textContent = 'The computer wins! Cry about it.';
+        }
     }
-    else if (ref(round) === 'lose'){
-        yourPoints++;
+
+
+messageDiv.appendChild(message);
+
     }
-}
-if (yourPoints > myPoints) {
-    return `Computer wins! ${yourPoints} to ${myPoints}!`
-}
-else if (myPoints > yourPoints){
-    return `You win! ${myPoints} to ${yourPoints}!`;
-} 
-else if (myPoints === yourPoints){
-    return `Nobody wins! ${yourPoints} to ${myPoints}! How lame.`
-}
+
+);
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', (e) => {
+    e.target.classList.add('pushed');
+    const messageDiv = document.querySelector('.results');
+
+        if(document.querySelector('.results>p')){
+        messageDiv.removeChild(document.querySelector('.results>p'));
+        };
+
+    const message = document.createElement('p');
+    message.textContent = playRound('paper');
+
+    if (ref(message.textContent) === 'win'){
+        let yourPoints = document.querySelector('.points-left');
+
+        yourPoints.textContent = '0' + `${yourScore}`;
+        yourScore++;
+        if(yourScore === 6){
+        message.textContent = 'You win the game!'
+        }
+
+    }      
+    if (ref(message.textContent) === 'lose'){
+        let compPoints = document.querySelector('.points-right');
+
+        compPoints.textContent = '0' + `${computerScore}`;
+        computerScore++;
+
+        if(computerScore === 6){
+        message.textContent = 'The computer wins! Cry about it.';
+        }
+    }
+
+
+messageDiv.appendChild(message);
+
+    }
+
+);
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', (e) => {
+    e.target.classList.add('pushed');
+    const messageDiv = document.querySelector('.results');
+
+        if(document.querySelector('.results>p')){
+        messageDiv.removeChild(document.querySelector('.results>p'));
+        };
+
+    const message = document.createElement('p');
+    message.textContent = playRound('scissors');
+
+    if (ref(message.textContent) === 'win'){
+        let yourPoints = document.querySelector('.points-left');
+
+        yourPoints.textContent = '0' + `${yourScore}`;
+        yourScore++;
+        if(yourScore === 6){
+        message.textContent = 'You win the game!'
+
+
+        }
+
+    }      
+    if (ref(message.textContent) === 'lose'){
+        let compPoints = document.querySelector('.points-right');
+
+        compPoints.textContent = '0' + `${computerScore}`;
+        computerScore++;
+
+        if(computerScore === 6){
+        message.textContent = 'The computer wins! Cry about it.';
+        }
+    }
+
+
+messageDiv.appendChild(message);
+
+    }
+
+);
+
+
+const endTransition = e => {
+    if (e.propertyName !== 'transform'){
+        return;
+    }
+    selectRock.classList.remove('pushed');
 }
 
-console.log(game());
+const selectRock = document.querySelector('button#rock');
+selectRock.addEventListener('transitionend', endTransition);
+
+
+const endPaperTransition = e => {
+    if (e.propertyName !== 'transform'){
+        return;
+    }
+    selectPaper.classList.remove('pushed');
+}
+
+const selectPaper = document.querySelector('button#paper');
+selectPaper.addEventListener('transitionend', endPaperTransition);
+
+
+const endScisTransition = e => {
+    if (e.propertyName !== 'transform'){
+        return;
+    }
+    selectScis.classList.remove('pushed');
+}
+
+const selectScis = document.querySelector('button#scissors');
+selectScis.addEventListener('transitionend', endScisTransition);
+
